6d33d730dbc0b96b2335c44260d112e3961c0ef0
Steve Kariuki <stevekariuki5@gmail.com>
2023-02-12 14:12:41 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    def initialize word
+        @word = word
+    end
+
+    def match arr
+       x = arr.map {|q| if(@word.chars.sort == q.chars.sort)
+         q
+       end
+    }
+    x.compact
+    end
+end
